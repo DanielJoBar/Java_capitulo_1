0964e9462a34d399d41ec01046dc42b8155e524f
DanielJoBar <jodar.barbero.daniel@gmail.com>
2022-10-06 09:44:22 +0200

    2nd commit

---
+++ Ejercicio2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+* Muestra por pantalla la frase "¡Hola mundo!"
+*
+*/
+
+import java.util.Scanner;
+public class Ejercicio2 { // Clase principal
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in); 
+    System.out.print("Introduzca su nombre u apodo:  ");
+    String name =sc.next();
+    System.out.print("Introduzca su dirección:   ");
+    String direction = sc.next();
+    System.out.print("Introduzca su número de teléfono:   ");
+    String telphone = sc.next();
+    System.out.println("Bienvenido Sr/a    "+name);
+    System.out.println("Con teléfono:   "+telphone);
+    System.out.println("Con dirección:   "+direction);
+    
+}}
