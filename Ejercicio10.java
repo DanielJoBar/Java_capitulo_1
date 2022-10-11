1bf0353b0c466f559ce79f5334583b1da56f065f
DanielJoBar <jodar.barbero.daniel@gmail.com>
2022-10-11 10:30:41 +0200

    Bolo morado

---
+++ Ejercicio10.java
@@ -0,0 +1,16 @@
+public class Ejercicio10{
+    public static void main(String[] args) {
+    String PURPLE = "\033[35m";
+    String White = "\033[37m";
+    System.out.println (PURPLE+"   *********  ");
+    System.out.println(PURPLE+" *************** ");
+    System.out.println(PURPLE+"***   ****    ***");
+    System.out.println(PURPLE+"*****************");
+    System.out.println(PURPLE+"*******    ******");
+    System.out.println(PURPLE+"  *************");
+    System.out.println(PURPLE+"    *********");
+    System.out.println(White+"               ");
+
+
+    }
+}
